c66298f89bc9ee86773c3f697f63c348f3f4af8d
prakasgouda <prakashbik@gmail.com>
2019-09-08 17:13:20 +0530

    Angular 8 application
    Angular 8 application

---
+++ angtest/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  myText = 'Hello world !';
+
+  subText='Welcome to Angular 8.'
+}
